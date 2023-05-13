7d6b40d856f96740f22fc004af0becef5a3b5e49
DavidK1412 <68391937+DavidK1412@users.noreply.github.com>
2023-05-12 21:58:24 -0500

    DAO and DTO firsts implementations

---
+++ src/java/modelo/dao/EmpleadoDAO.java
@@ -0,0 +1,34 @@
+package modelo.dao;
+
+import interfaces.IDao;
+import java.util.ArrayList;
+import modelo.dto.EmpleadoDTO;
+
+public class EmpleadoDAO implements IDao<EmpleadoDTO> {
+
+    @Override
+    public EmpleadoDTO search(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<EmpleadoDTO> listAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean delete(EmpleadoDTO obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean update(EmpleadoDTO obj, String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean create(EmpleadoDTO obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
